f87a67369bf0ed9e0f50f120b2c80eb906ec18b5
s-da-sailor <akashlanard07@gmail.com>
2022-04-07 19:04:04 +0600

    Issue2: Implement Better File Structure

--- app.js
+++ app.js
@@ -14,163 +14,17 @@
 * Date: 5 April 2022
 */
 
-// dependencies
+// DEPENDENCIES
 const express = require('express');
+const storyRouter = require('./routes/storyRoutes');
 
-// express application
+// APPLICATION
 const app = express();
 
-// middlewares
-app.use(express.json());
+// MIDDLEWARES
+app.use(express.json());                        // for getting JSON request body
 
-// CRUD Endpoints for Story (@TODO handle this using a separate Router later)
+app.use('/api/v1/stories', storyRouter);        // using story Router
 
-//dummy data for stories
-const stories  = [
-  {
-    id: 0,
-    title: "Dummy Title 1",
-    description: "Dummy Description 1",
-    author: "Dummy author 1",
-    createdAt: Date.now()
-  },
-  {
-    id: 1,
-    title: "Dummy Title 2",
-    description: "Dummy Description 2",
-    author: "Dummy author 2",
-    createdAt: Date.now()
-  },
-  {
-    id: 2,
-    title: "Dummy Title 3",
-    description: "Dummy Description 3",
-    author: "Dummy author 3",
-    createdAt: Date.now()
-  }
-];
-
-// Story controllers (@TODO shift them in controller directory later)
-
-// controller for getting all the stories
-const getAllStories = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: stories.length,
-    data: {                                     // serve dummy stories array
-      stories
-    }
-  });
-};
-
-// controller for getting a specific story
-const getStory = (req, res) => {
-  const id = Number(req.params.id);             // convert string to number
-  const story = stories.find(s => s.id === id); // find story having the specified ID
-
-  if(!story) {                                  // if the story is not found return 404
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({                        // return the story as JSON 
-    status: 'success',
-    data: {                                     // serve from dummy data
-      story
-    }
-  });
-};
-
-// controller for creating a story
-const createStory = (req, res) => {
-  const newId = stories[stories.length - 1].id + 1;         // get newID 
-  const newStory = Object.assign({ id: newId }, req.body);  // dummy story object
-  stories.push(newStory);                                   // pushed in the dummy array
-  res.status(201).json({
-    status: 'success',
-    data: {                                     // create in db and return (to be done later)
-      story: newStory
-    }
-  });
-};
-
-// controller for updating a story (partial payload)
-const updateStoryPatch = (req, res) => {
-  const id = Number(req.params.id);             // convert string to number
-  const story = stories.find(s => s.id === id); // find story having the specified ID
-
-  if(!story) {                                  // if the story is not found return 404
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({                        // return the story as JSON 
-    status: 'success',                          
-    data: {                                     // update in db and return (to be done later)
-      story: '<Updated story here..>'
-    }
-  });
-};
-
-// controller for updating a story (full payload)
-const updateStoryPut = (req, res) => {
-  const id = Number(req.params.id);             // convert string to number
-  const story = stories.find(s => s.id === id); // find story having the specified ID
-
-  if(!story) {                                  // if the story is not found return 404
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({                        // return the story as JSON 
-    status: 'success',                          
-    data: {                                     // update in db and return (to be done later)
-      story: '<Updated story here..>'
-    }
-  });
-};
-
-// controller for deleting a story
-const deleteStory = (req, res) => {
-  const id = Number(req.params.id);             // convert string to number
-  const story = stories.find(s => s.id === id); // find story having the specified ID
-
-  if(!story) {                                  // if the story is not found return 404
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(204).json({                        // body is empty (no content)
-    status: 'success',                          
-    data: null                                  // delete from db (to be done later)
-  });
-};
-
-// Story Router (@TODO transfer it in a different directory)
-const storyRouter = express.Router();
-
-// routes (@TODO transfer it in a different directory)
-storyRouter
-  .route('/')
-  .get(getAllStories)
-  .post(createStory);
-
-storyRouter
-  .route('/:id')
-  .get(getStory)
-  .patch(updateStoryPatch)
-  .put(updateStoryPut)
-  .delete(deleteStory);
-
-app.use('/api/v1/stories', storyRouter);        // using story Router as a middleware
-
-// export the application
+// EXPORT
 module.exports = app;
