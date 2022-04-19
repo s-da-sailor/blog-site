bce528783e700d168e4aa0a9fbc1d7b2de193d7b
s-da-sailor <akashlanard07@gmail.com>
2022-04-19 15:03:47 +0600

    Issue4: Integrate ESLint and Prettier

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 /*
-* Title: Database Configuration
-* Description: This application returns a sequelize instance using database configuration
-* Author: Akash Lanard
-* Date: 11 April 2022
-*/
+ * Title: Database Configuration
+ * Description: This application returns a sequelize instance using database configuration
+ * Author: Akash Lanard
+ * Date: 11 April 2022
+ */
 
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize(
-  'blog_site',                    // Database name
-  'root',                         // Username
-  '',                             // Password
+  'blog_site', // Database name
+  'root', // Username
+  '', // Password
   {
-    host: 'localhost',            // Host
-    dialect: 'mysql',             // Dialect
+    host: 'localhost', // Host
+    dialect: 'mysql', // Dialect
     //logging: false                // logging
   }
-); 
+);
