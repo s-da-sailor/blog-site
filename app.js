8cac16a2b25c3f777178403bf95e3f7666ff9ca3
s-da-sailor <akashlanard07@gmail.com>
2022-04-20 15:04:47 +0600

    Issue6: Remove Unnecessary Comments

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 /*
  * Title: blog-site application (to be changed later)
- * Description: A javascript application for a simple blog site having following functionalities:
- *             1. In the home page there will be a list of stories posted by different authors
- *             containing title, description, author name, created at etc.
- *             2. When clicked at any of the stories we will be redirected to a seperate page
- *             where we will be able to read the full story.
- *             3. Users can register to this web site. Registered users will be able to log in
- *             using their username and password. Registered users will be able to post, update and delete stories.
- *             4. Everyone (logged in or anonymous) will be able to read the stories. Anonymous users will be able to only read
- *             the stories. Registered users will be able to post stories and update/delete stories which are posted
- *             by the same user
- * Author: Akash Lanard
- * Date: 5 April 2022
+ * Description: A javascript application for a simple blog site
  */
 
 // DEPENDENCIES
--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-/*
- * Title: Server Sub-App
- * Description: This application starts a server and listens on a specefied port
- * Author: Akash Lanard
- * Date: 6 April 2022
- */
-
 // DEPENDENCIES
 const app = require('./app');
 
-// PORT (defined in configuration file or 8000 if not defined)
 const port = process.env.PORT || 8000;
 
-// start the server on the specified port
 app.listen(port, () => {
   console.log(`listening to port ${port}...`);
 });
