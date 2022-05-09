deb6c43050b216e53a9ddba7ce58a4c2c8d5991d
s-da-sailor <akashlanard07@gmail.com>
2022-05-09 11:17:57 +0600

    Issue11: Add Tests for authController signup

--- tests/unitTest/controllers/authController.test.js
+++ tests/unitTest/controllers/authController.test.js
@@ -2,6 +2,17 @@ const { mockRequest, mockResponse } = require('mock-req-res');
 const jwt = require('jsonwebtoken');
 const authController = require('../../../controllers/authController');
 const contentNegotiation = require('../../../utils/contentNegotiation');
+const User = require('../../../models/userModel');
+
+const mockResponseUserCreate = {
+  dataValues: {
+    username: 'dummyUsername3',
+    name: 'Dummy Name C',
+    email: 'dummyemail3@dummymail.com',
+    password: 'dummypassword3',
+    passwordConfirm: 'dummypassword3',
+  },
+};
 
 describe('Test signToken', () => {
   test('Sign a token with proper parameters', () => {
@@ -62,5 +73,51 @@ describe('Test createAndSendToken', () => {
     expect(authController.signToken).toHaveBeenCalledTimes(1);
     expect(mockRes.cookie).toHaveBeenCalledTimes(1);
     expect(contentNegotiation.serveData).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('Test signup', () => {
+  test('Signup a new user with 201 status', async () => {
+    jest.clearAllMocks();
+
+    const mockReq = mockRequest({
+      body: {
+        username: 'dummyUsername3',
+        name: 'Dummy Name C',
+        email: 'dummyemail3@dummymail.com',
+        password: 'dummypassword3',
+        passwordConfirm: 'dummypassword3',
+      },
+    });
+    const mockRes = mockResponse();
+    const mockNext = jest.fn();
+
+    jest.spyOn(User, 'create').mockImplementation((user) => {
+      expect(user.username).toBe('dummyUsername3');
+      expect(user.name).toBe('Dummy Name C');
+      expect(user.email).toBe('dummyemail3@dummymail.com');
+      expect(user.password).toBe('dummypassword3');
+      expect(user.passwordConfirm).toBe('dummypassword3');
+      return mockResponseUserCreate;
+    });
+
+    jest
+      .spyOn(authController, 'createAndSendToken')
+      .mockImplementation((user, statusCode, req, res, next) => {
+        expect(user.username).toBe('dummyUsername3');
+        expect(user.name).toBe('Dummy Name C');
+        expect(user.email).toBe('dummyemail3@dummymail.com');
+        expect(user.password).toBeFalsy();
+        expect(user.passwordConfirm).toBeFalsy();
+        expect(statusCode).toBe(201);
+        expect(req).toEqual(mockReq);
+        expect(res).toEqual(mockRes);
+        expect(next).toEqual(mockNext);
+      });
+
+    await authController.signup(mockReq, mockRes, mockNext);
+
+    expect(User.create).toHaveBeenCalledTimes(1);
+    expect(authController.createAndSendToken).toHaveBeenCalledTimes(1);
   });
 });
