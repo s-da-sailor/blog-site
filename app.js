b805d89b7e76739e2b02218271d139cc4bc52fc6
s-da-sailor <akashlanard07@gmail.com>
2022-06-08 14:22:31 +0600

    Changed origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 
 // CORS OPTIONS
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   optionsOnSuccess: 200,
